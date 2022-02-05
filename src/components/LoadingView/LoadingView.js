3cebcd3250076763b9a8557dcbf43249e93409b3
jesus <macbookpro@MacBook-Pro.local>
2022-02-04 21:48:22 -0600

    loading view retrieve email

--- src/components/LoadingView/LoadingView.js
+++ src/components/LoadingView/LoadingView.js
@@ -1,22 +1,32 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppTabs from '_navigator/AppTabs';
 import RNBootSplash from 'react-native-bootsplash';
 import {Welcome} from '../../screens/HomeScreens/Welcome/Welcome';
 import AuthStack from '_navigator/stacks/AuthStack';
 import {useUser} from '_store/hooks/useUser';
+import Store from '_utils/helpers/store';
+import {locallyStoredUserVariables} from '_utils/constans/Constants';
 
 const LoadingView = () => {
+  const [storedEmail, setStoredEmail] = useState(null);
   const {isUserLoggedIn} = useUser();
 
+  const getEmail = async () => {
+    const email = await Store.get(locallyStoredUserVariables.USER_STORED_EMAIL);
+    setTimeout(() => {
+      RNBootSplash.hide({fade: true});
+    }, 500);
+    if (email) setStoredEmail(email);
+  };
+
   useEffect(() => {
-    RNBootSplash.hide({fade: true});
+    getEmail();
     return () => {};
   }, []);
-
   let content = (
     <NavigationContainer>
-      {isUserLoggedIn ? <AppTabs /> : <AuthStack />}
+      {isUserLoggedIn || storedEmail ? <AppTabs /> : <AuthStack />}
     </NavigationContainer>
   );
 
